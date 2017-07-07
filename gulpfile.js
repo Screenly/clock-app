b2a74d190a2c8b83603a81d7d4c5909e5792ab41
Peter Monte <monte.peter@gmail.com>
2017-07-07 18:18:59 +0100

    cache buster
    I got everything to work except for any css and js replacement. So for
    the moment background will not work.

--- gulpfile.js
+++ gulpfile.js
@@ -12,16 +12,14 @@ var folder = {
         svg: source + 'svg/**/*',
         fonts: source + 'fonts/**/*',
         css: source + 'css/*.scss',
-        css_vendor: source + 'css/vendor/',
         js: source + 'js/*.js',
-        js_vendor: source + 'js/vendor/',
     },
     build: {
         img: build + 'img/',
         svg: build + 'svg/',
         fonts: build + 'fonts/',
-        css: build + 'css',
-        js: build + 'js'
+        css: build + 'css/',
+        js: build + 'js/'
     }
 }
 
@@ -45,80 +43,10 @@ var prefix = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 var sourceMaps = require('gulp-sourcemaps');
 var imageMin = require('gulp-imagemin');
+var rename = require("gulp-rename");
+var intercept = require('gulp-intercept');
 var rev = require('gulp-rev');
-
-
-
-
-
-
-/**
- * Styles task. This includes the app custom styles plus any vendor library
- */
-
-gulp.task('css', function() {
-
-    // CLEAN ASSETS
-    del.sync([folder.build.css], { read: false });
-
-    // VENDOR
-    gulp.src([folder.source.css_vendor + 'normalize.css'])
-        .pipe(prefix())
-        .pipe(cleanCSS())
-        .pipe(minify())
-        .pipe(concat('vendor.css'))
-        .pipe(rev())
-        .pipe(gulp.dest(folder.build.css));
-
-    // APP CSS FILES
-    return gulp.src(folder.source.css)
-        .pipe(sourceMaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(sourceMaps.write(''))
-        .pipe(rev())
-        .pipe(gulp.dest(folder.build.css));
-});
-
-gulp.task('default', ['css']);
-
-
-
-
-
-
-/**
- * Scripts task. This includes the app custom scripts plus any vendor library
- */
-
-gulp.task('js', function() {
-
-    // CLEAN ASSETS
-    del.sync([folder.build.js], { read: false });
-
-    // VENDOR
-    gulp.src([
-            folder.source.js_vendor + 'moment-with-locales.min.js',
-            folder.source.js_vendor + 'moment-timezone-with-data.js'
-        ])
-        .pipe(concat('vendor.js'))
-        .pipe(rev())
-        .pipe(gulp.dest(folder.build.js));
-
-    // APP JS FILES
-    return gulp.src(folder.source.js)
-        .pipe(jshint())
-        .pipe(sourceMaps.init())
-        .pipe(uglify())
-        .pipe(sourceMaps.write(''))
-        .pipe(flatten())
-        .pipe(rev())
-        .pipe(gulp.dest(folder.build.js));
-});
-
-gulp.task('default', ['css', 'js']);
-
-
+var revReplace = require('gulp-rev-replace');
 
 
 
@@ -127,44 +55,75 @@ gulp.task('default', ['css', 'js']);
  * Static assets task
  */
 
-gulp.task('static', function() {
+gulp.task('assets', function() {
 
-    // CLEAN ASSETS
+    // CLEAN
     del.sync([
         folder.build.img,
-        folder.build.svg,
-        folder.build.fonts
+        folder.build.css,
+        folder.build.js,
+        'rev-manifest.json'
     ], { read: false });
 
-    // VENDOR
-
-    var img = gulp.src(folder.source.img)
+    // IMAGES
+    return gulp.src(folder.source.img)
         .pipe(imageMin())
         .pipe(rev())
-        .pipe(gulp.dest(folder.build.img));
+        .pipe(gulp.dest(folder.build.img))
+        .pipe(rev.manifest({ base: build, merge: true }))
+        .pipe(gulp.dest(build))
+        .on('end', function() {
 
-    var svg = gulp.src(folder.source.svg)
-        .pipe(imageMin([imageMin.svgo({ plugins: [{ removeViewBox: true }] })]))
-        .pipe(rev())
-        .pipe(gulp.dest(folder.build.svg));
+            console.log ('DONE IMAGES');
 
-    var fonts = gulp.src(folder.source.fonts)
-        .pipe(rev())
-        .pipe(gulp.dest(folder.build.fonts));
+            // CSS
+            gulp.src(folder.source.css)
+                .pipe(sourceMaps.init())
+                .pipe(sass().on('error', sass.logError))
+                .pipe(cleanCSS())
+                .pipe(rev())
+                .pipe(sourceMaps.write(''))
+                .pipe(gulp.dest(folder.build.css))
+                .pipe(rev.manifest({ base: build, merge: true }))
+                .pipe(gulp.dest(build))
+                .on('end', function() {
+            
+                    console.log ('DONE CSS');
 
-    return img && svg && fonts;
+                    // JS
+                    gulp.src(folder.source.js)
+                        .pipe(jshint())
+                        .pipe(sourceMaps.init())
+                        .pipe(uglify())
+                        .pipe(rev())
+                        .pipe(sourceMaps.write(''))
+                        .pipe(gulp.dest(folder.build.js))
+                        .pipe(rev.manifest({ base: build, merge: true }))
+                        .pipe(gulp.dest(build))
+                        .on('end', function() {
+
+                            console.log ('DONE JS');
+
+                            var manifest = gulp.src("rev-manifest.json");
+
+                            // CSS MANIFEST
+                            gulp.src(folder.build.css + '*.css')
+                            .pipe(revReplace({manifest: manifest}))
+                            .pipe(gulp.dest(folder.build.css));
+
+                            // CSS MANIFEST
+                            gulp.src(folder.build.js + '*.js')
+                            .pipe(revReplace({manifest: manifest}))
+                            .pipe(gulp.dest(folder.build.js));
+
+                            // HTML MANIFEST
+                            gulp.src(source + "index.html")
+                            .pipe(revReplace({manifest: manifest}))
+                            .pipe(gulp.dest('./'));
+                        });
+                });
+        });
 });
-
-
-
-
-
-
-/**
- * So that we can just call `gulp` in terminal
- */
-
-gulp.task('default', ['css', 'js', 'static']);
 
 
 
@@ -174,5 +133,13 @@ gulp.task('default', ['css', 'js', 'static']);
  * Add tasks to the Watch event
  */
 gulp.task('watch', function() {
-    gulp.watch(source + '**/*', ['css', 'js', 'static']);
+    gulp.watch(source + '**/*', ['assets']);
 });
+
+
+
+
+/**
+ * So that we can just call `gulp` in terminal
+ */
+gulp.task('default', ['assets']);
