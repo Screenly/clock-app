cfd7d7aa97e0078f4ed0e57c3b91bffa09af577e
Viktor Petersson <vpetersson@wireload.net>
2017-07-10 21:35:04 +0100

    Clean up on deploy.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ TARGET="45.55.204.101"
 TARGET_USER="deployer"
 TARGET_PATH="/www/clock.apps.screenlyapp.com"
 
-rsync -aP index.html assets ${TARGET_USER}@${TARGET}:${TARGET_PATH}
+rsync -aP --delete index.html assets ${TARGET_USER}@${TARGET}:${TARGET_PATH}
