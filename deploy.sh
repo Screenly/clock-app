52b6b1f65fd0f5c6b7f44962fa46be3e3e761281
Viktor Petersson <vpetersson@wireload.net>
2017-07-11 12:45:20 +0100

    Minor production tweaks.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 TARGET="45.55.204.101"
 TARGET_USER="deployer"
-TARGET_PATH="/www/clock.apps.screenlyapp.com"
+TARGET_PATH="/www/clock.srly.io/"
 
-rsync -aP --delete index.html assets ${TARGET_USER}@${TARGET}:${TARGET_PATH}
+rsync -aP --delete _site/* ${TARGET_USER}@${TARGET}:${TARGET_PATH}
